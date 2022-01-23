8fa459a857f9b7fa69199dbfa32ff7e86c134022
Cadenza <mirandalucsb@gmail.com>
2022-01-23 15:33:38 -0700

    added question code/function skeletons to index.js

---
+++ index.js
@@ -0,0 +1,156 @@
+const inquirer = require('inquirer');
+const db = require('mysql2');
+const table = require('console.table');
+const { allowedNodeEnvironmentFlags } = require('process');
+
+const questions = [
+    {
+        type: 'list',
+        name: 'action',
+        message: 'What would you like to do?',
+        choices: ['View all departments','View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee role']
+    }
+];
+
+const promptUser = () => {
+    return inquirer.prompt(questions);
+}
+
+const viewAllDepartments = () => {
+    //use the db to run a sql query
+    //use console.table to print the results of the query
+    //return promptUser.
+    //db.query();
+    return promptUser();
+}
+
+const viewAllRoles = () => {
+    //use the db to run a sql query
+    //usd console.table to print the results of the query
+    //return promptUser
+    return promptUser();
+}
+
+const viewAllEmpoyees = () => {
+    //use the db to run a sql query
+    //usd console.table to print the results of the query
+    //return promptUser
+    return promptUser();
+}
+
+const addDepartment = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'depName',
+            message: 'What is the name of the department?'
+        }
+    ])
+    .then(data => {
+        //make a query that adds the department here!
+        return promptUser();
+    })
+
+}
+
+const addRole = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'roleName',
+            message: 'What is the name of the role?'
+        },
+        {
+            type: 'input',
+            name: 'roleSalary',
+            message: 'What is the salary for this role?'
+        },
+        {
+            type: 'input',
+            name: 'roleDep',
+            message: 'What is the department of this role?'
+
+        }
+    ])
+    .then(data => {
+        //make a query that adds the role here!
+        return promptUser();
+    });
+}
+
+const addEmployee = () =>{
+    return inquirer.prompt([
+        {
+//employee’s first name, last name, role, and manager
+            type: 'input',
+            name: 'empFirstName',
+            message: 'What is the first name of this employee?'
+        },
+        {
+            type: 'input',
+            name: 'empLastName',
+            message: 'What is the last name of this employee?'
+        },
+        {
+            type: 'input',
+            name: 'empRole',
+            message: 'What is the role of this employee?'
+        },
+        {
+            type: 'input',
+            name: 'empMan',
+            message: 'Who is the Manager of this employee?'
+        }
+    ])
+    .then(data => {
+        //make a query that adds the employee here!
+        return promptUser();
+    })
+}
+
+const updateEmployeeRole = () => {
+    //make choices array by using a sql query here!
+    let choices = [];
+    return inquirer.prompt([
+        {
+            type: 'list',
+            name: 'employee',
+            message: 'Select an employee to update:',
+            choices: choices
+        }
+    ])
+    .then(data => {
+        //make a query that updates the employee role here!
+        return promptUser();
+    })
+
+}
+
+const init = () => {
+    promptUser()
+    .then((data) => {
+        if(data.action === "View all departments"){
+            viewAllDepartments();
+        }
+        else if(data.action === "View all roles"){
+            viewAllRoles();
+        }
+        else if(data.action === 'View all employees'){
+            viewAllEmpoyees();
+        }
+        else if(data.action === 'Add a department'){
+            addDepartment();
+        }
+        else if(data.action === 'Add a role'){
+            addRole();
+        }
+        else if(data.action === 'Add an employee'){
+            addEmployee();
+        }
+        else if(data.action === 'Update an employee role'){
+            updateEmployeeRole();
+        }
+    })
+}
+
+init();
