d6998e9cbbce0773603153cd4bf2826ae92e5e07
Cadenza <mirandalucsb@gmail.com>
2022-01-29 13:21:03 -0700

    made tweaks to inquirer questions and printed existing data in add functionality

--- db/schema.sql
+++ db/schema.sql
@@ -10,8 +10,8 @@ CREATE TABLE department (
 CREATE TABLE role (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
-    salary DECIMAL(10, 2),
-    department_id INTEGER,
+    salary DECIMAL(10, 2) NOT NULL,
+    department_id INTEGER NOT NULL,
     FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
 );
 
@@ -19,8 +19,8 @@ CREATE TABLE employee (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INTEGER,
-    manager_id INTEGER,
+    role_id INTEGER NOT NULL,
+    manager_id INTEGER NOT NULL,
     FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL,
     FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
 );
