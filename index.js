8d0eca30e38153c26365cb146acc3c0d1b3622e4
Cadenza <mirandalucsb@gmail.com>
2022-01-29 13:58:43 -0700

    added logo file and welcome message.

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 const userChoices = require('./lib/userChoices');
+const fs = require('fs');
 
-//call user choices
-userChoices.userChoices();
+fs.readFile("EMS.txt","utf8", function(err,data){
+    if(err){
+        console.log(err);
+    }
+    console.log(data);
+    console.log("------Employee Management Systems------");
+    console.log(".");
+    console.log(".");
+    console.log("------WELCOME!------");
+    console.log(".");
+    console.log(".");
+    userChoices.userChoices();
+});
